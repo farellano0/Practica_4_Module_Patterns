9cc3c963f1e75cef0dc3ce07410dd032ccb3303e
Fernando Arellano <farellano0@ucol.mx>
2022-02-26 16:38:16 -0600

    persona.domicilio

--- objects/app.js
+++ objects/app.js
@@ -3,6 +3,12 @@ let persona = {
     apellido: "Ulibarri",
     getName: function () {
         return `${this.nombre} ${this.apellido}`
+    },
+    domicilio: {
+        calle: "Madero #144",
+        colonia: "Centro",
+        cp: 28000,
+        municipio: "Colima"
     }
 }
-console.log(persona.getName());
+console.log(persona.domicilio);
