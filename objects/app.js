875c69fbc3ec156189529f2d7877fb4cccc8ae81
Fernando Arellano <farellano0@ucol.mx>
2022-02-26 16:37:33 -0600

    persona: getName()

--- objects/app.js
+++ objects/app.js
@@ -1,5 +1,8 @@
 let persona = {
     nombre: "Carlos",
     apellido: "Ulibarri",
+    getName: function () {
+        return `${this.nombre} ${this.apellido}`
+    }
 }
 console.log(persona.getName());
