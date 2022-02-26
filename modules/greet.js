d03daeec5e2934d33e9de9e391098ccdcce35cec
Fernando Arellano <farellano0@ucol.mx>
2022-02-26 16:22:44 -0600

    function and require

--- modules/greet.js
+++ modules/greet.js
@@ -1,1 +1,5 @@
-console.log('Hello from the module'); //Se manda a llamar en la consola el texto que esta entre comillas simples
+//console.log('Hello from the module'); //Se manda a llamar en la consola el texto que esta entre comillas simples.
+
+//Se convierte el mensaje de saluda a una función mediante un function expression.
+let greet = () => console.log('Hello from the module');
+greet();
